a2c362e91cfafea2a6c346499555a609e5e79a49
Dhamu143 <ankurp0608@gmail.com>
2026-03-03 15:46:45 +0530

    chnges

--- server.js
+++ server.js
@@ -31,9 +31,13 @@ app.use(
 
 // 🔴 YOU FORGOT THIS
 app.use(express.json());
-// Nodemailer Transporter
+// Nodemailer Transporter\\\\
+
+
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    secure: process.env.SMTP_SECURE === 'true',
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
